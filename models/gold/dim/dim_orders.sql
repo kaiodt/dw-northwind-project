e04a3c28acf6548052300a0d2b467a76d54e1737
Kaio Rocha <kaiodtr@gmail.com>
2025-04-15 21:51:22 -0300

    add technical rows to dim_orders model

--- models/gold/dim/dim_orders.sql
+++ models/gold/dim/dim_orders.sql
@@ -35,6 +35,42 @@ final AS (
 
   FROM
     source
+  
+  UNION ALL
+
+  -- Technical row for records where order_id is NULL
+
+  SELECT
+    '-1' AS order_sk,
+    -1 AS order_id,
+    'Unknown Name' AS ship_to_name,
+    'Unknown Address' AS ship_to_address,
+    'Unknown City' AS ship_to_city,
+    'Unknown Region' AS ship_to_region,
+    'Unknown Postal Code' AS ship_to_postal_code,
+    'Unknown Country' AS ship_to_country,
+    -1 AS freight,
+    CAST('1900-01-01' AS DATETIME) AS last_modified,
+    CAST('1900-01-01' AS DATETIME) AS silver_loaded_at,
+    CAST(SYSDATETIME() AS DATETIME) AS gold_loaded_at
+
+  UNION ALL
+
+  -- Technical row for records where customer_id is not found
+
+  SELECT
+    '-2' AS order_sk,
+    -2 AS order_id,
+    'Unregistered Name' AS ship_to_name,
+    'Unknown Address' AS ship_to_address,
+    'Unknown City' AS ship_to_city,
+    'Unknown Region' AS ship_to_region,
+    'Unknown Postal Code' AS ship_to_postal_code,
+    'Unknown Country' AS ship_to_country,
+    -1 AS freight,
+    CAST('1900-01-01' AS DATETIME) AS last_modified,
+    CAST('1900-01-01' AS DATETIME) AS silver_loaded_at,
+    CAST(SYSDATETIME() AS DATETIME) AS gold_loaded_at
 )
 
 SELECT * FROM final;
